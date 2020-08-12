7f91194b0495e6a235695638b74f91167b887d1f
heyj <heyj@zetyun.com>
2020-08-12 14:53:44 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,13 @@
+# d = {'lilee': 25, 'wangyan': 21, 'liqun': 32, 'age': 19}
+#
+# print(d.items())
+# print(type(d.items()))
+# a=lambda item:item[1]
+# print(a(d.items()))
+
+d = {'lilee': 25, 'wangyan': 21, 'liqun': 32, 'age': 19}
+c = sorted(d.items(), key=lambda item: item[1])
+a = {}
+for i in c:
+    a[i[0]] = i[1]
+print(a)
